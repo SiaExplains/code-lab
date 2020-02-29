617d22f8a540f26a1ea81a36bfbcdd1b6cb48d08
sia.qnbr@gmail.com <sia.qnbr@gmail.com>
2020-02-29 17:56:11 +0100

    sync programming in js & use callbacl for async prog.

---
+++ javascript/async/sync-problem.js
@@ -0,0 +1,23 @@
+const posts = [
+    { title: 'post one', body: 'body of first post' },
+    { title: 'post two', body: 'body of second post' }
+];
+
+function getPosts() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach(p => {
+            output += `* ${p.title}\r\n`;
+        });
+        console.log(output);
+    }, 1000);
+}
+
+function createPost(post) {
+    setTimeout(() => {
+        posts.push(post);
+    }, 2000);
+}
+
+createPost({ title: 'post three', body: 'body of third post' });
+getPosts();
