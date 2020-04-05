558e4eee84a5bbbf785f23803f4d719af8e846b5
sia.qnbr@gmail.com <sia.qnbr@gmail.com>
2020-04-05 14:53:01 +0200

    add command and singleton pattern

--- patterns/command/command.js
+++ patterns/command/command.js
@@ -1,3 +1,30 @@
 /*
     pattern not implemented yet!
+
+    a single entry point to do some actions!
 */
+
+class Manager {
+    constructor() {
+        this.items = [];
+    }
+
+    do(name, params = null) {
+        let funcName = '_' + name;
+        let args = Array.from(arguments);
+        if (funcName) this[funcName](args[1]);
+    }
+
+    _add(item) {
+        this.items.push(item);
+    }
+
+    _print() {
+        console.log(this.items);
+    }
+}
+
+let mng = new Manager();
+mng.do('add', ['ali', 'sia']);
+mng.do('add', 'reza');
+mng.do('print');
