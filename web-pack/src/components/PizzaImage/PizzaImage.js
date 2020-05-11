2e2cd727638a78399b713a983073528da91477d4
sia.qnbr@gmail.com <sia.qnbr@gmail.com>
2020-05-11 17:06:59 +0200

    working on webpack configuration

---
+++ web-pack/src/components/PizzaImage/PizzaImage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './PizzaImage.css';
+import PizzaImage from '../../asstes/pizza.jpg';
+
+const pizzaImage = (props) => {
+    return (
+        <div className={style.PizzaImage}>
+            <img src={PizzaImage} className={style. } />
+        </div>
+    );
+};
+
+export default pizzaImage;
